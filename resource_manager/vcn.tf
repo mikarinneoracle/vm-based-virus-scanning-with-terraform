49622f808d4e5c8bace79a64494220df3232c9f0
Mika Rinne <mika.rinne@oracle.com>
2023-02-28 13:20:38 +0200

    fix

--- resource_manager/vcn.tf
+++ resource_manager/vcn.tf
@@ -34,42 +34,6 @@ resource "oci_core_route_table" "scanning_private_rt" {
   }
 }
 
-resource "oci_core_security_list" "scanning_security_list_private" {
-  compartment_id = var.compartment_id
-  vcn_id         = oci_core_vcn.scanning_vcn.id
-  display_name   = "Security list for scanning private subnet"
-
-  egress_security_rules {
-    destination      = "0.0.0.0/0"
-    protocol         = "all"
-    destination_type = "CIDR_BLOCK"
-  }
-
-  ingress_security_rules {
-    protocol = "1"
-    source   = "0.0.0.0/0"
-    icmp_options {
-      type = "3"
-      code = "4"
-    }
-    source_type = "CIDR_BLOCK"
-  }
-
-  ingress_security_rules {
-    protocol = "6"
-    source   = "0.0.0.0/0"
-    tcp_options {
-      max = "22"
-      min = "22"
-    }
-    source_type = "CIDR_BLOCK"
-  }
-
-  freeform_tags = {
-    Managed = var.tags
-  }
-}
-
 resource "oci_core_nat_gateway" "scanning_nat_gw" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.scanning_vcn.id
@@ -89,9 +53,6 @@ resource "oci_core_subnet" "Private_Subnet_scanning" {
   freeform_tags       = {
     Managed = var.tags
   }
-  security_list_ids = [
-     oci_core_security_list.scanning_security_list_private.id
-  ]
   prohibit_internet_ingress  = "true"
   prohibit_public_ip_on_vnic = "true"
   route_table_id             = oci_core_route_table.scanning_private_rt.id

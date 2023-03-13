77fc38dd9b335334bc376837dad628b298ad5fdf
Mika Rinne <mika.rinne@oracle.com>
2023-03-13 12:16:29 +0200

    tf modified

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -25,6 +25,6 @@ resource "oci_objectstorage_preauthrequest" "scanning_preauth" {
   bucket       = oci_objectstorage_bucket.scanning.name
   name         = "scanning_preauth"
   namespace    = data.oci_objectstorage_namespace.user_namespace.namespace
-  object_name  = null
+  object       = null
   time_expires = timeadd(timestamp(), "8765h")
 }

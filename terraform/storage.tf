2d04ba0dc896d77d99e4c2739f6a4033b6038f26
Mika Rinne <mika.rinne@oracle.com>
2023-03-13 12:32:05 +0200

    tf modified

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -18,20 +18,3 @@ resource "oci_objectstorage_bucket" "scanning_alert_report" {
   namespace      = data.oci_objectstorage_namespace.user_namespace.namespace
   object_events_enabled = true
 }
-
-resource "oci_objectstorage_preauthrequest" "scanning_preauth" {
-  access_type  = "ObjectWrite"
-  bucket       = oci_objectstorage_bucket.scanning.name
-  name         = "scanning_preauth"
-  namespace    = data.oci_objectstorage_namespace.user_namespace.namespace
-  object_name  = oci_objectstorage_object.dummy.object
-  time_expires = timeadd(timestamp(), "8765h")
-}
-
-resource "oci_objectstorage_object" "dummy" {
-  #Required
-  bucket    = oci_objectstorage_bucket.scanning.name
-  content   = null
-  namespace = data.oci_objectstorage_namespace.user_namespace.namespace
-  object    = "dummy"
-}

708ae26757b1232e09015fc5cb7f467a21644ed4
Mika Rinne <mika.rinne@oracle.com>
2024-11-26 10:03:02 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,6 +2,7 @@ export OCI_CLI_AUTH=instance_principal
 rm -f /home/opc/report.txt
 rm -rf /home/opc/scandir
 mkdir /home/opc/scandir
+# Using namespace is optional in oci cli commands below
 namespace=$(oci os ns get | jq .data | tr -d '"')
 oci os object bulk-download --bucket-name scanning --region eu-amsterdam-1 --download-dir /home/opc/scandir --namespace $namespace
 /usr/local/uvscan/uvscan -v --unzip --analyze --summary --afc 512 --program --mime --recursive --threads=$(nproc) \
@@ -9,11 +10,16 @@ oci os object bulk-download --bucket-name scanning --region eu-amsterdam-1 --dow
 isInFile=$(cat /home/opc/report.txt | grep -c "Possibly Infected:.............     0")
 if [ $isInFile -eq 0 ]; then
    echo "################# ALERT!!! Scanning found infected files ! #################"
-   oci os object put --bucket-name scanning-alert-report --region eu-amsterdam-1 --file /home/opc/report.txt --force
+   oci os object put --bucket-name scanning-alert-report --region eu-amsterdam-1 --file /home/opc/report.txt --force --namespace $namespace
 else
    echo "################# Scanning found no infected files #################"
-   oci os object bulk-delete --bucket-name scanned --region eu-amsterdam-1 --force
-   oci os object put --bucket-name scanned --region eu-amsterdam-1 --file /home/opc/report.txt --force
-   oci os object bulk-upload --bucket-name scanned --region eu-amsterdam-1 --src-dir /home/opc/scandir
+   oci os object bulk-delete --bucket-name scanned --region eu-amsterdam-1 --force --namespace $namespace
+   oci os object put --bucket-name scanned --region eu-amsterdam-1 --file /home/opc/report.txt --force --namespace $namespace
+   #oci os object bulk-upload --bucket-name scanned --region eu-amsterdam-1 --src-dir /home/opc/scandir --namespace $namespace 
+   ls /home/opc/scandir > scanned.out
+   while read file; do
+    echo "uploading $file to scanned files"
+    oci os object put --bucket-name scanned --region eu-amsterdam-1 --file /home/opc/scandir/$file --force --namespace $namespace
+   done <scanned.out
 fi
-oci os object bulk-delete --bucket-name scanning --region eu-amsterdam-1 --force
+oci os object bulk-delete --bucket-name scanning --region eu-amsterdam-1 --force --namespace $namespace
